02b023e80168abc9c219587b0153d7405feb4c18
Filipp Voronov <fivoronov@gmail.com>
2022-07-01 20:30:56 +0300

    Initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @Test
+    public void test() {
+
+    }
+}
